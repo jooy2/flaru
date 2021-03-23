5b9e7d22d9dfa28f176ac509810541b82bcaae10
leejooy96 <leejooy96@gmail.com>
2021-03-23 17:48:29 +0900

    swf file associate for electron builder

--- app/index.js
+++ app/index.js
@@ -1,12 +1,54 @@
 const {
-  app, BrowserWindow, protocol, shell,
+  app, BrowserWindow, protocol, shell, ipcMain, dialog,
 } = require('electron');
-
-const isDev = !app.isPackaged;
 const path = require('path');
 const electronLocalShortcut = require('electron-localshortcut');
+const Store = require('electron-store');
+const fs = require('fs');
+const pkg = require('../package.json');
+require('@electron/remote/main').initialize();
 
+global.APP_VERSION_NAME = pkg.version;
+global.APP_VERSION_CODE = pkg.versionCode;
+global.APP_RUFFLE_VERSION_DATE = pkg.ruffleVersionDate;
+const MAX_RECENT_FILES = 5;
+
+const isDev = !app.isPackaged;
+const isWindows = process.platform === 'win32';
+const store = new Store();
 let win;
+
+function createSettings() {
+  if (store.size < 1) {
+    store.set({
+      firstScreen: 'explorer',
+      recentFiles: [],
+    });
+  }
+}
+
+function getRecentFiles() {
+  const recentFiles = store.get('recentFiles');
+  if (!recentFiles) {
+    store.set({
+      recentFiles: [],
+    });
+    return [];
+  }
+  return recentFiles;
+}
+
+const fileExists = async p => !!(await fs.promises.stat(p).catch(() => false));
+
+const openFromExplorer = (argv, argvIndex = 1) => {
+  if (isWindows && argv && argv.length >= argvIndex
+      && argv[argvIndex]?.indexOf('.swf') !== -1) {
+    win.webContents.send('receiveNextRenderer', argv[argvIndex]);
+    win.webContents.send('receiveOpenFile', argv[argvIndex]);
+  } else {
+    win.webContents.send('receiveNextRenderer');
+  }
+};
 
 function createWindow() {
   win = new BrowserWindow({
@@ -19,21 +61,25 @@ function createWindow() {
       spellcheck: false,
       webSecurity: false,
       devTools: process.env.NODE_ENV === 'dev',
+      contextIsolation: false,
+      enableRemoteModule: true,
     },
   });
   win.setMenuBarVisibility(false);
   win.loadURL(isDev ? 'http://localhost:9090' : `file://${path.join(__dirname, '../build/index.html')}`).catch(e => {
     console.log(e);
   }).then(() => {
-    if (isDev) {
-      win.webContents.openDevTools();
-    }
+    if (isDev) win.webContents.openDevTools();
   });
   win.on('focus', () => {
     electronLocalShortcut.register(win, ['CommandOrControl+R', 'CommandOrControl+Shift+R', 'F5'], () => {});
   });
   win.on('blur', () => {
     electronLocalShortcut.unregisterAll(win);
+  });
+  win.webContents.once('dom-ready', () => {
+    const { argv } = process;
+    openFromExplorer(argv, 1);
   });
   win.webContents.on('new-window', async (event, url) => {
     event.preventDefault();
@@ -48,11 +94,16 @@ const gotTheLock = app.requestSingleInstanceLock();
 if (!gotTheLock) {
   app.quit();
 } else {
-  app.on('second-instance', () => {
+  app.on('second-instance', (e, argv) => {
     if (win) {
       if (win.isMinimized()) win.restore();
       win.focus();
+      openFromExplorer(argv, 2);
     }
+  });
+  app.on('open-file', (event, pathParam) => {
+    event.preventDefault();
+    if (!isWindows) win.webContents.send('receiveOpenFile', pathParam);
   });
   if (process.platform === 'darwin') {
     app.dock.hide();
@@ -62,6 +113,7 @@ if (!gotTheLock) {
       const pathname = decodeURI(request.url.replace('file:///', ''));
       callback(pathname);
     });
+    createSettings();
     createWindow();
   });
   app.on('window-all-closed', () => {
@@ -72,6 +124,62 @@ if (!gotTheLock) {
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
+    }
+  });
+
+  ipcMain.on('getAppConfig', () => {
+    win.webContents.send('receiveAppConfig', store.store);
+  });
+
+  ipcMain.on('getRecentFiles', () => {
+    win.webContents.send('receiveRecentFiles', getRecentFiles().reverse());
+  });
+
+  ipcMain.on('setAppConfig', (event, args) => {
+    store.set(args);
+  });
+
+  ipcMain.on('resetAppConfig', () => {
+    store.clear();
+    app.relaunch();
+    app.exit();
+  });
+
+  ipcMain.on('appendRecentFiles', (event, file) => {
+    try {
+      const recentFiles = getRecentFiles();
+      const fileIndex = recentFiles.findIndex(x => x === file);
+      if (fileIndex !== -1) recentFiles.splice(fileIndex, 1);
+      recentFiles.push(file);
+      if (recentFiles.length > MAX_RECENT_FILES) recentFiles.shift();
+      store.set({ recentFiles });
+    } catch (e) {
+      store.set({
+        recentFiles: [],
+      });
+    }
+  });
+
+  ipcMain.on('checkFileExist', async (event, args) => {
+    win.webContents.send('receiveFileExist', {
+      name: args.name,
+      path: args.path,
+      exist: await fileExists(args.path),
+    });
+  });
+
+  ipcMain.on('removeRecentFile', async (event, args) => {
+    const response = await dialog.showMessageBox(win, {
+      type: 'info',
+      title: args.title,
+      message: args.message,
+    });
+    if (response && response.response < 2) {
+      const recentFiles = getRecentFiles();
+      const fileIndex = recentFiles.findIndex(x => x === args.path);
+      if (fileIndex !== -1) recentFiles.splice(fileIndex, 1);
+      store.set({ recentFiles });
+      win.webContents.send('receiveRecentFiles', recentFiles.reverse());
     }
   });
 }
