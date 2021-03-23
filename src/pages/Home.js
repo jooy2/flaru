141942824825d8f12b0ed42b512192bf946736cf
leejooy96 <leejooy96@gmail.com>
2021-03-23 17:49:11 +0900

    separate player and explore from home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,124 +1,71 @@
-import React, {
-  useState, useCallback,
-} from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import {
-  Grid, LinearProgress, Paper, Typography,
+  Grid, LinearProgress, Typography,
 } from '@material-ui/core';
-import { useDropzone } from 'react-dropzone';
-import { makeStyles } from '@material-ui/core/styles';
 import { useTranslation } from 'react-i18next';
 
+import { useHistory } from 'react-router-dom';
+import * as configActions from '../store/modules/config';
 import Layout from '../components/layouts/Layout';
-import FlashPlayer from '../components/player/FlashPlayer';
-import AppMenu from '../components/layouts/AppMenu';
-import AppAlert from '../components/views/AppAlert';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    padding: theme.spacing(1, 0),
-  },
-  paper: {
-    padding: theme.spacing(2),
-    marginBottom: theme.spacing(2),
-  },
-  dropzone: {
-    cursor: 'pointer',
-    userSelect: 'none',
-    padding: theme.spacing(3, 5),
-    minHeight: '280px',
-    textAlign: 'center',
-    fontStyle: 'italic',
-    borderRadius: '5px',
-    border: theme.palette.type === 'light' ? '3px dashed #333' : '3px dashed #fff',
-  },
-}));
+const Home = ({ ConfigActions }) => {
+  const [t] = useTranslation(['common']);
+  const history = useHistory();
+  const electron = window.require('electron');
+  const { ipcRenderer } = electron;
 
-const Home = () => {
-  const classes = useStyles();
-  const [t] = useTranslation(['common', 'notice', 'menu']);
-  const [filePath, setFilePath] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [flashContentError, setFlashContentError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
-  const onDrop = useCallback(async acceptedFiles => {
-    const file = acceptedFiles[0];
-    const fileType = file?.type;
-    const uploadFilePath = file?.path;
-    const blob = fileType ? fileType.split('/')[1] : null;
-    if (blob !== 'x-shockwave-flash'
-        && blob !== 'futuresplash'
-        && blob !== 'x-shockwave-flash2-preview'
-        && blob !== 'vnd.adobe.flash-movie') {
-      setFlashContentError(true);
-      setErrorMessage(t('notice:wrong-file-type'));
-      return false;
-    }
-    setLoading(true);
-    setFilePath(uploadFilePath);
-    setLoading(false);
-    return true;
+  const runFromExplorer = async (filePath) => {
+    await ConfigActions.setConfig({
+      flashFileName: filePath.split('\\').pop(),
+      flashFilePath: filePath,
+    });
+    ipcRenderer.send('appendRecentFiles', filePath);
+    history.push('/player');
+  };
+
+  const loadOrMove = (filePath) => {
+    ipcRenderer.on('receiveAppConfig', async () => {
+      ipcRenderer.on('receiveOpenFile', async (openFileEvent, receivePath) => {
+        await runFromExplorer(receivePath);
+      });
+      if (filePath) {
+        await runFromExplorer(filePath);
+      } else {
+        history.push('/explorer');
+      }
+    });
+    ipcRenderer.send('getAppConfig');
+  };
+
+  useEffect(() => {
+    ipcRenderer.on('receiveNextRenderer', (nextRendererEvent, filePath) => {
+      loadOrMove(filePath);
+    });
+
+    return () => {
+      ipcRenderer.removeAllListeners('receiveAppConfig');
+      ipcRenderer.removeAllListeners('receiveNextRenderer');
+    };
   }, []);
-  const { getRootProps, getInputProps } = useDropzone({
-    accept: '.swf',
-    multiple: false,
-    onDrop,
-  });
 
   return (
     <Layout
-      title={filePath ? t('player-title') : t('main-title')}
-      withTail={!filePath}
-      withBackButton={filePath}
-      withHelpButton={!filePath}
-      container={!filePath}
+      header={false}
     >
-      <div className={filePath ? {} : classes.root}>
-        {!filePath && !loading
-            && (
-            <>
-              <Grid item xs={12}>
-                <Paper className={classes.paper}>
-                  <div {...getRootProps({ className: classes.dropzone })}>
-                    <Typography component="h2" variant="subtitle1">
-                      {t('notice:drag-drop-execute')}
-                    </Typography>
-                    <input {...getInputProps()} />
-                  </div>
-                  {flashContentError
-                  && (
-                  <Grid item xs={12}>
-                    <AppAlert
-                      severity="error"
-                      withMargin
-                      text={errorMessage}
-                    />
-                  </Grid>
-                  )}
-                </Paper>
-              </Grid>
-              <Grid item xs={12}>
-                <AppMenu />
-              </Grid>
-            </>
-            )}
-        {loading
-            && (
-            <Grid item xs={12}>
-              <LinearProgress />
-              {t('player-loading')}
-            </Grid>
-            )}
-        {!loading && filePath
-            && (
-            <Grid item xs={12}>
-              <FlashPlayer
-                filePath={filePath}
-              />
-            </Grid>
-            )}
-      </div>
+      <Grid item xs={12}>
+        <LinearProgress />
+        <Typography component="p">
+          <strong>{t('loading')}</strong>
+        </Typography>
+      </Grid>
     </Layout>
   );
 };
 
-export default Home;
+const mapDispatchToProps = dispatch => ({
+  ConfigActions: bindActionCreators({ ...configActions }, dispatch),
+});
+
+export default connect(null, mapDispatchToProps)(Home);
