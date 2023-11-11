1c92bbcb440aee8d733ad74b1fb28985332139b6
leejooy96 <leejooy96@gmail.com>
2023-11-11 11:30:25 +0900

    change playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,5 +2,10 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   outputDir: 'tests/results',
-  retries: 3,
+  retries: process.env.E2E ? 2 : 0,
+  workers: process.env.E2E ? 1 : undefined,
+  timeout: 60000,
+  expect: {
+    timeout: 10000,
+  },
 });
